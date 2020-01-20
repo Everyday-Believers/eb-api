9c2e502644eb212d3698be9dcc636040e96cd5f4
jjarl12 <jonathan-jarl@bethel.edu>
2020-01-19 19:26:31 -0600

    Update mongo db to use environment variable

--- config/config.js
+++ config/config.js
@@ -6,5 +6,5 @@ module.exports = {
 	 */
 	FRONT_URL: "http://127.0.0.1:8000", // for the content of the mails with link to ... (in forgot-password)
 	secretOrKey: "secret", // db password, which must be complicated.
-	mongoURI: "mongodb://127.0.0.1:27017/for-auth", // db url & table
+	mongoURI: process.env.MONGO_URL, // db url & table
 };
