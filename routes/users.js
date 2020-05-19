446542b3d2693e674aa129ece21468f4592bb5ce
serhiy1017 <s.bezpalyi@yandex.com>
2020-05-20 01:05:41 +0800

    added organiztion name to User model

--- routes/users.js
+++ routes/users.js
@@ -484,6 +484,32 @@ router.post("/update", (req, res) => {
 						return res.status(500).json({msg_name: "Database error."});
 					});
 			}
+			else if(req.body.organization_name !== undefined){
+				if(isEmpty(req.body.organization_name)){
+					return res.status(400).json({msg_phone: "Organization name is required."});
+				}
+				else if(user.organization_name === req.body.organization_name){
+					return res.status(200).json({msg_phone: "Not modified!"});
+				}
+				else{
+					User.findOne({organization_name: req.body.organization_name}).then(usr => {
+						if(usr){
+							return res.status(400).json({msg_phone: "The organization name was already registered."});
+						}
+						else{
+							user.organization_name = req.body.organization_name;
+							user.save()
+								.then(() => {
+									// modified
+									return res.status(200).json({msg_phone: "Modified!"});
+								})
+								.catch(() => {
+									return res.status(500).json({msg_phone: "Database error."});
+								});
+						}
+					});
+				}
+			}
 			else if(req.body.admin_email !== undefined){
 				if(isEmpty(req.body.admin_email) && isEmpty(user.phone)){
 					return res.status(400).json({msg_admin_email: "Email OR phone is required."});
